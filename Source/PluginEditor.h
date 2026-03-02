397d732a317b74557ca2b9cc4209294cb9986a6e
nadahong99 <youho1036@gmail.com>
2026-03-02 14:11:56 +0900

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,5 +1,8 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_dsp/juce_dsp.h>
 #include "PluginProcessor.h"
 
 // ─── Settings Screen (API Key) ────────────────────────────────────────────────
